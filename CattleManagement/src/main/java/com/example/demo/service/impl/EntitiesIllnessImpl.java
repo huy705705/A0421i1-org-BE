f422eac14c5b743de82e3ba62ab7e1e924190eff
A0421I1-Nguyen Duc Tung <nductung2705@gmail.com>
2022-04-20 18:50:40 +0700

    ngu thi xoa

--- CattleManagement/src/main/java/com/example/demo/service/impl/EntitiesIllnessImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service.impl;
-
-import com.example.demo.service.EntitiesIllnessService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class EntitiesIllnessImpl implements EntitiesIllnessService {
-}
