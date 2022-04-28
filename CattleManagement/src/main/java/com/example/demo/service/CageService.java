47bbb77ecd581aea8f9be1ec1c2b8f9a50addf51
A0421I1-Nguyen Duc Tung <nductung2705@gmail.com>
2022-04-28 15:00:40 +0700

    clear dev BE

--- CattleManagement/src/main/java/com/example/demo/service/CageService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-
-import java.util.List;
-
-public interface CageService {
-    List<String> getListCageId();
-
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# A0421i1-org-BE
-Project Quản lí dự án chăn nuôi gia súc ( phần BE)
