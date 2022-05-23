e8d551b131f1ebadbf1dace1d4897dca261c2f42
A0421I1_Nguyen_Pham_Quoc_Huy <huuyquoc@gmail.com>
2022-05-23 15:49:53 +0700

    huynpq

---
+++ CattleManagement/src/main/java/com/example/demo/service/IGeneralNotificationService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface IGeneralNotificationService<T> {
+    Page<T> findAll(Pageable pageable);
+}
